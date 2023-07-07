a28bff78b13ce1f5abbdd91c1e50f1f3bbdd1518
drone0898 <drone0898@gmail.com>
2023-07-08 02:18:01 +0900

    init

---
+++ app/src/main/java/toss/next/drone0898/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package toss.next.drone0898.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import toss.next.drone0898.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
